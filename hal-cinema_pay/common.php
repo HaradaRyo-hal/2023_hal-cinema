195cf4c4a0fe3d744b01ef0d29bb24c2b8570b78
HaradaRyo-hal <harada0111hal@gmail.com>
2023-07-07 17:32:31 +0900

    a

--- hal-cinema_pay/common.php
+++ hal-cinema_pay/common.php
@@ -1,23 +1,7 @@
-<!-- process_form.php -->
 <?php
-// フォームデータを取得
-$lastName = $_POST['lastName'];
-$firstName = $_POST['firstName'];
-$email = $_POST['email'];
-$emailConfirmation = $_POST['emailConfirmation'];
-$securityCode = $_POST['securityCode'];
-$cardHolderName = $_POST['cardHolderName'];
-
-// フォームデータを次のページに渡すための処理を実行する（例：セッション変数を使用する）
-session_start();
-$_SESSION['lastName'] = $lastName;
-$_SESSION['firstName'] = $firstName;
-$_SESSION['email'] = $email;
-$_SESSION['emailConfirmation'] = $emailConfirmation;
-$_SESSION['securityCode'] = $securityCode;
-$_SESSION['cardHolderName'] = $cardHolderName;
-
-// 次のページにリダイレクト
-header("Location: ./step4.php");
-exit();
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $email = $_POST['email'];
+    $phoneNumber = $_POST['phoneNumber'];
+    header("Location: step4.html?lastName=$lastName&firstName=$firstName&email=$email&phoneNumber=$phoneNumber");
 ?>
