48ac89572c4cf75db2ce8382db08209f940ba617
HaradaRyo-hal <harada0111hal@gmail.com>
2023-07-14 09:30:54 +0900

    test

--- src/components/main/toppage.jsx
+++ src/components/main/toppage.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect , useState } from 'react';
+import React from 'react';
 import './toppage.css';
 import SlideShowComponent from './toppage_component/SlideShow';
 import News_Component from './toppage_component/news';
 import ScheduleComponent from './toppage_component/schedule';
 
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-
 const Home = () => {
 
   return (
-      <main>
-        <SlideShowComponent/>
-        <News_Component/>
-        <ScheduleComponent/>
+    <main>
+      <SlideShowComponent/>
+      <News_Component/>
+      <ScheduleComponent/>
     </main>
   );
 }
